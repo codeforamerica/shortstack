7d517a7d63202247fedb5128d28c3d4c582c59dd
Rocky Meza <rockymeza@gmail.com>
2011-01-21 11:19:14 -0700

    starting rspec stuff

---
+++ config/initializers/rspec_generator.rb
@@ -0,0 +1,6 @@
+Orgbase::Application.configure do
+  config.generators do |g|
+    g.integration_tool :rspec
+    g.test_framework   :rspec
+  end
+end if defined? Orgbase::Application
---
+++ spec/controllers/authentication_spec.rb
@@ -0,0 +1,2 @@
+describe AuthenticationsController do
+end
