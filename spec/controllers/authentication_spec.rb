236b8ec5ba1740ad643b2b168c65a7a8e7a13cc9
Rocky Meza <rockymeza@gmail.com>
2011-01-21 13:34:37 -0700

    authentication spec that does not throw up

--- spec/controllers/authentication_spec.rb
+++ spec/controllers/authentication_spec.rb
@@ -1,2 +1,14 @@
-describe AuthenticationsController do
-end
+require 'spec_helper'
+
+#describe AuthenticationsController do
+#  describe 'GET destroy' do
+#    it 'destroys an omniauth authentication' do
+#      get :destroy
+#    end
+#
+#    it 'redirects to authentications_url' do
+#      get :destroy
+#      response.should redirect_to authentications_url
+#    end
+#  end
+#end
