d9bff5996e5d763fc086eac7616f3e4105416e74
Rocky Meza <rockymeza@gmail.com>
2011-01-21 16:16:40 -0700

    forgot to add actual spec file

---
+++ spec/controllers/addresses_spec.rb
@@ -0,0 +1,51 @@
+#spec/controllers/addresss_spec.rb
+require 'spec_helper'
+
+describe AddressesController do
+
+  before do
+    @admin = Factory(:user)
+    Factory(:person)
+    sign_in @admin
+  end
+
+  describe '#create' do
+     before do
+       post :create, :address => Factory.attributes_for(:address)
+     end
+     
+     it "should create a address" do
+       Address.all.size.should be 1
+     end
+     
+     it "should have a parent object" do
+       Address.first.addressable_type.should == 'Person'
+     end
+   end
+   
+   describe '#update' do
+     before do
+       @address = Factory(:address)
+       put :update, :id => @address.id, :address => { :address => "123 Elm Street" }
+     end
+   
+     it "should update a address" do
+       # pending "should specify update action"
+       @address.reload.address.should == "123 Elm Street"
+     end
+   end
+   
+   describe '#destroy' do
+     before do
+       @address = Factory(:address)
+       @address_count = Address.all.size
+       delete :destroy, :id => @address.id
+     end
+   
+     it "should destroy a address" do
+       # pending "should specify destroy action"
+       Address.all.size.should == @address_count - 1
+       expect{@address.reload}.to raise_error ActiveRecord::RecordNotFound
+     end
+   end
+end
