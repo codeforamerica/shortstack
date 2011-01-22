3159311668128e2a6a750e3f6d9f5c6250e0cb19
Dan Melton <melton.dan@gmail.com>
2011-01-21 18:04:22 -0800

    Adding host for produciton email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,5 +23,6 @@ Orgbase::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,5 +49,6 @@ Orgbase::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true  
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'short-stack.heroku.com' }
   
 end
