1e4125b9022bd5263251ab153ce7a7cea7cb9467
Dan Melton <melton.dan@gmail.com>
2011-01-21 18:01:37 -0800

    Added gmail plugin to send email on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Orgbase::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.delivery_method = :smtp
+  
 end
