96c0140b6aec4958ce89a1f88ddc3036a638eb4f
Pete Fecteau <pete@buttonpresser.com>
2011-01-25 09:16:18 -0800

    flash notice css change

--- db/schema.rb
+++ db/schema.rb
@@ -13,47 +13,47 @@
 ActiveRecord::Schema.define(:version => 20110122203721) do
 
   create_table "addresses", :force => true do |t|
-    t.string    "address"
-    t.string    "city"
-    t.string    "state"
-    t.string    "zipcode"
-    t.string    "country"
-    t.string    "lat"
-    t.string    "long"
-    t.string    "addressable_type"
-    t.integer   "addressable_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zipcode"
+    t.string   "country"
+    t.string   "lat"
+    t.string   "long"
+    t.string   "addressable_type"
+    t.integer  "addressable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "authentications", :force => true do |t|
-    t.integer   "user_id"
-    t.string    "provider"
-    t.string    "uid"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "contacts", :force => true do |t|
-    t.string    "phone"
-    t.string    "email"
-    t.string    "twitter"
-    t.string    "facebook"
-    t.string    "linkedin"
-    t.string    "github"
-    t.integer   "contactable_id"
-    t.string    "contactable_type"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.string   "linkedin"
+    t.string   "github"
+    t.integer  "contactable_id"
+    t.string   "contactable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "contributions", :force => true do |t|
-    t.integer   "user_id"
-    t.integer   "contributable_id"
-    t.string    "contributable_type"
-    t.string    "action"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "user_id"
+    t.integer  "contributable_id"
+    t.string   "contributable_type"
+    t.string   "action"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "delayed_jobs", :force => true do |t|
@@ -72,103 +72,103 @@ ActiveRecord::Schema.define(:version => 20110122203721) do
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "link_types", :force => true do |t|
-    t.string    "name"
-    t.string    "description"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "links", :force => true do |t|
-    t.string    "name"
-    t.text      "link_url"
-    t.integer   "linkable_id"
-    t.string    "linkable_type"
-    t.integer   "link_type_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.text     "link_url"
+    t.integer  "linkable_id"
+    t.string   "linkable_type"
+    t.integer  "link_type_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "note_types", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "notes", :force => true do |t|
-    t.string    "name"
-    t.text      "note"
-    t.integer   "noteable_id"
-    t.string    "noteable_type"
-    t.integer   "note_type_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.text     "note"
+    t.integer  "noteable_id"
+    t.string   "noteable_type"
+    t.integer  "note_type_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "org_types", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "organizations", :force => true do |t|
-    t.string    "name"
-    t.integer   "org_type_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.integer  "org_type_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "people", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "products", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", :force => true do |t|
-    t.integer   "user_id"
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "relation_types", :force => true do |t|
-    t.string    "name"
-    t.string    "type_name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.boolean   "parent",     :default => false
+    t.string   "name"
+    t.string   "type_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "parent",     :default => false
   end
 
   create_table "relationships", :force => true do |t|
-    t.string    "relation_type"
-    t.string    "parentable_type"
-    t.integer   "parentable_id"
-    t.string    "childable_type"
-    t.integer   "childable_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "relation_type"
+    t.string   "parentable_type"
+    t.integer  "parentable_id"
+    t.string   "childable_type"
+    t.integer  "childable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "settings", :force => true do |t|
-    t.string    "name"
-    t.string    "setting"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.string   "setting"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "taggings", :force => true do |t|
-    t.integer   "tag_id"
-    t.integer   "taggable_id"
-    t.string    "taggable_type"
-    t.integer   "tagger_id"
-    t.string    "tagger_type"
-    t.string    "context"
-    t.timestamp "created_at"
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
   end
 
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
@@ -179,38 +179,38 @@ ActiveRecord::Schema.define(:version => 20110122203721) do
   end
 
   create_table "users", :force => true do |t|
-    t.string    "email",                               :default => "", :null => false
-    t.string    "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string    "password_salt",                       :default => "", :null => false
-    t.string    "reset_password_token"
-    t.string    "remember_token"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",                       :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "whisk_types", :force => true do |t|
-    t.string    "name"
-    t.string    "setting"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.string   "setting"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "whisks", :force => true do |t|
-    t.string    "setting"
-    t.integer   "whiskable_id"
-    t.string    "whiskable_type"
-    t.integer   "whisk_type_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "setting"
+    t.integer  "whiskable_id"
+    t.string   "whiskable_type"
+    t.integer  "whisk_type_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
