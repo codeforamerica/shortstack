ff63c4fd69f5435d49eae995288235257931d31b
Dan Melton <melton.dan@gmail.com>
2011-02-04 14:01:08 -0800

    Added raise exception for addaddress

--- app/models/add_address.rb
+++ app/models/add_address.rb
@@ -9,16 +9,20 @@ class AddAddress
       if organization.addresses.blank?
         setting = organization.whisks.where(:whisk_type_id => 4)
         if !setting.blank?
-        longlat = setting.first.setting.split(", ")
-        html = Net::HTTP.get(URI.parse('http://maps.googleapis.com/maps/api/geocode/json?latlng='+longlat[1]+','+longlat[0]+'&sensor=false'))
-        s = JSON.parse(html)
-        if s['status'] != "ZERO_RESULTS"
-          f = s['results'][0]['address_components'].detect { |f| f['types'].include?("administrative_area_level_1")}
-          if f
-          puts organization.name
-          organization.addresses.new(:state => f["short_name"], :lat => longlat[1], :long => longlat[0]).save
+          begin
+            longlat = setting.first.setting.split(", ")
+            html = Net::HTTP.get(URI.parse('http://maps.googleapis.com/maps/api/geocode/json?latlng='+longlat[1]+','+longlat[0]+'&sensor=false'))
+            s = JSON.parse(html)
+            if s['status'] != "ZERO_RESULTS"
+                f = s['results'][0]['address_components'].detect { |f| f['types'].include?("administrative_area_level_1")}
+                if f
+                  puts organization.name
+                  organization.addresses.new(:state => f["short_name"], :lat => longlat[1], :long => longlat[0]).save
+                end
+            end
+          rescue 
+           puts "Failed to work for #{organization.name}"
           end
-        end
         end
       end
     end
