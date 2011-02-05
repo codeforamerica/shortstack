88c213b824a1037583c4dac4448e45144b512161
Rocky Meza <rockymeza@gmail.com>
2011-02-04 18:30:31 -0700

    livesearching, it is kinda bad though

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(function()
+{
+
+  var quick_search_form = $('#quick_search_form');
+
+  if ( quick_search_form )
+  {
+    quick_search_form.find('input').keyup(function(event)
+    {
+      $.get(quick_search_form[0].action, quick_search_form.serialize(), null, 'script');
+    });
+  }
+});
