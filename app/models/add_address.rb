6f3f313dad19875a13b7666957593dd14532c8f2
Dan Melton <melton.dan@gmail.com>
2011-02-04 17:14:06 -0800

    Added a script to alter names for cities and counties

--- app/models/add_address.rb
+++ app/models/add_address.rb
@@ -26,5 +26,22 @@ class AddAddress
         end
       end
     end
-end
+  end
+  
+  def change_name
+    orgs = Organization.where(:org_type_id => 4) || Organization.where(:org_type_id => 8)
+    orgs.each do |organization|
+      if !organization.addresses.blank?
+          begin
+            state = organization.addresses.first.state
+            if !organization.name.include?(state)
+              organization.update_attributes(:name => organization.name + ", #{state}")
+            end
+          rescue 
+           puts "Failed to work for #{organization.name}"
+          end
+      end
+    end 
+  end 
+
 end
