787402b32ca0d708d74928f03758f899c480ef88
Dan Melton <melton.dan@gmail.com>
2011-02-11 12:05:05 -0800

    Process improvements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!  
+  before_filter :authenticate_user!
+  caches_page :index
+    
   def index
-    @contributions = Contribution.order('updated_at DESC').limit(10)
+    @contributions = Contribution.order('updated_at DESC').limit(10).includes(:user =>  :profile)
     @people = Person.recent.limit(10)
     @products = Product.recent.limit(10)    
     @organizations = Organization.recent.limit(10)        
