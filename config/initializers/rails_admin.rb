2b563d45d508c09788ed533001b16cd52dab858e
Rocky Meza <rockymeza@gmail.com>
2011-02-25 18:26:55 -0700

    made user edit page much faster

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,3 +4,16 @@ RailsAdmin.authenticate_with {
 RailsAdmin.authorize_with {
   authorize_user
 }
+
+RailsAdmin.config do |config|
+  config.model User do
+    edit do 
+      group(:Authentications) do
+        hide
+      end
+      group(:Contributions) do
+        hide
+      end
+    end
+  end
+end
