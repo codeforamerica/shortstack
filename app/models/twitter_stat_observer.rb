4f78dbe8a9522866d2161b8b02da3e1fd3eb3b57
Dan Melton <melton.dan@gmail.com>
2011-04-15 12:59:58 -0700

    Fixing observer for twitter stat

--- app/models/twitter_stat_observer.rb
+++ app/models/twitter_stat_observer.rb
@@ -5,9 +5,9 @@ class TwitterStatObserver < ActiveRecord::Observer
     #update the attributes
     if !summary.blank?
     #or create new Twit summary
-    summary.update_attributes(:followers_count => stat.followers_count  , :following_count => stat.following_count, :listed_count => stat.listed_count, :statuses_count => stat.statuses_count )
+    summary.update_attributes(:followers_count => stat.followers_count, :following_count => stat.following_count, :listed_count => stat.listed_count, :statuses_count => stat.statuses_count )
     else
-      TwitterSummary.create(:organization_id => stat.link.linkable, :link => stat.link, :followers_count => stat.followers_count  , :following_count => stat.following_count, :listed_count => stat.listed_count, :statuses_count => stat.statuses_count )
+      TwitterSummary.create(:organization => stat.link.linkable, :link => stat.link, :followers_count => stat.followers_count  , :following_count => stat.following_count, :listed_count => stat.listed_count, :statuses_count => stat.statuses_count )
     end  
   end
 end
