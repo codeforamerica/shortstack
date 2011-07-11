33cc2026da28d997f571f813f54529353f13f326
Dan Melton <melton.dan@gmail.com>
2011-07-11 15:57:20 -0700

    Altered mongo production config:

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,7 +1,3 @@
-
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "mongo_test_#{Rails.env}"
-
 MongoMapper.config = {
  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] ||
                          'mongodb://localhost/shortstack' } }
