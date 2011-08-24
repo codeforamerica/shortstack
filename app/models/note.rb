95f0c11e6207678d57ebeeeddfa5ed1ea4c6a123
Dan Melton <melton.dan@gmail.com>
2011-08-24 14:01:42 -0700

    removed contributions from models

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,11 @@
 class Note < ActiveRecord::Base
   belongs_to :note_type
   belongs_to :noteable, :polymorphic => true
-  has_many :contributions, :as => :contributable, :class_name => "Contribution", :dependent => :destroy
-  after_update :update_contribution
-  after_create :create_contribution
 
   scope :about, :joins => :note_type, :conditions => ["note_types.name = 'about'"]
+  scope :requirements, :joins => :note_type, :conditions => ["note_types.name = 'requirements'"]  
+  scope :eula, :joins => :note_type, :conditions => ["note_types.name = 'end user license agreement'"]  
+  scope :permission, :joins => :note_type, :conditions => ["note_types.name = 'distribution permissions'"]    
 
   def create_contribution
     self.contributions << Contribution.new(:user =>$current_user, :action => "Create")
--- app/models/whisk.rb
+++ app/models/whisk.rb
@@ -2,16 +2,6 @@ class Whisk < ActiveRecord::Base
   belongs_to :whisk_type
   belongs_to :whiskable, :polymorphic => true
   has_many :contributions, :as => :contributable, :class_name => "Contribution", :dependent => :destroy
-  after_update :update_contribution
-  after_create :create_contribution
-
-  def create_contribution
-    self.contributions << Contribution.new(:user =>$current_user, :action => "Create")
-  end
-
-  def update_contribution
-    self.contributions << Contribution.new(:user =>$current_user, :action => "Update")
-  end
 
   def name
     self.whisk_type.name
