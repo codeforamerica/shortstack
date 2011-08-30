1124b37ed1664fa44c4afadb69089dc73fd9b6ef
Dan Melton <melton.dan@gmail.com>
2011-08-29 17:10:43 -0700

    Removing contributions from notes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @products }
+      format.json  { render :json => @products }      
     end
   end
 
