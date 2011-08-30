5e95647a50dd2e22ce6407434e399beb23fab795
Dan Melton <melton.dan@gmail.com>
2011-08-29 17:13:12 -0700

    Removing org_id migration:

--- db/migrate/20110824200408_add_org_id_to_products.rb
+++
@@ -1,9 +0,0 @@
-class AddOrgIdToProducts < ActiveRecord::Migration
-  def self.up
-    add_column :products, :organization_id, :integer
-  end
-
-  def self.down
-    remove_column :products, :organization_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110824200408) do
+ActiveRecord::Schema.define(:version => 20110429174911) do
 
   create_table "addresses", :force => true do |t|
     t.string    "address"
@@ -156,10 +157,9 @@ ActiveRecord::Schema.define(:version => 20110824200408) do
   end
 
   create_table "products", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "organization_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", :force => true do |t|
